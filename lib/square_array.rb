729886125daf37bda08a7a86dd938894459e5009
Bri Turner <blturner3527@gmail.com>
2020-01-09 15:50:59 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,5 +1,10 @@
 def square_array(array)
   counter = 0
+  new_array = []
+  
   while array[counter] do
-    
+    new_array.push(Math.sqrt(array[counter]))
+    counter += 1
+  end
+  new_array
 end
